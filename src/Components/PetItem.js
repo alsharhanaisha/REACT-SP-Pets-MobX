e632429816619f42d89fa6d83720ecaffcef95cb
alsharhanaisha <alsharhanaisha@gmail.com>
2022-01-24 20:41:17 +0300

    Done

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -1,15 +1,14 @@
-export default function PetItem({ pet, handleAdopt }) {
+import petStore from "../store/PetStore";
+
+function PetItem({ pet }) {
+  const adobt = () => petStore.handleAdopt(pet.id);
   return (
     <div class="col-lg-4 col-md-8 col-sm-10">
       <div class="single-doctor">
         <img className="image" alt={pet.name} src={pet.image} />
         <div class="content">
           <h3>{pet.name}</h3>
-          <button
-            type="button"
-            class="btn btn-info"
-            onClick={() => handleAdopt(pet.id)}
-          >
+          <button type="button" class="btn btn-info" onClick={adobt}>
             Adopt
           </button>
         </div>
@@ -17,3 +16,4 @@ export default function PetItem({ pet, handleAdopt }) {
     </div>
   );
 }
+export default PetItem;
